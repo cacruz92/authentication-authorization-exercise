0db98a9117629ef7ada1e5bf76bcbce2cd3ed764
Christian Cruz <christian.cruz92@hotmail.com>
2024-01-08 22:46:35 -0500

    added initial functionality code and created User model

---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    username = db.Column(db.Text, primary_key=True, autoincrement=True)
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, nullable=False, unique=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
