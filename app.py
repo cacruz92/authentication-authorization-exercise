0143f6e3956a679b0f1bdbe6ddc0552eeeef78ef
Christian Cruz <christian.cruz92@hotmail.com>
2024-01-10 23:33:16 -0500

    added the get and post route for the register page and made sure it was functional

--- app.py
+++ app.py
@@ -1,29 +1,56 @@
 from flask import Flask, render_template, redirect, session, flash
+from models import User, db, connect_db
+from forms import RegisterUser
+from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedbackapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "SECRET!"
+debug = DebugToolbarExtension(app)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 
-# Make routes for the following:
 
-# **GET */ :*** Redirect to /register.
+connect_db(app)
+
+with app.app_context():
+        db.drop_all()
+        db.create_all()
+
+
+
 
 @app.route('/')
 def show_homepage():
     return redirect('/register')
 
-@app.route('/register')
-def show_register_form():
-    return render_template('registerform.html')
+@app.route('/register', methods=['GET','POST'])
+def register_user():
+    """User registration form; handle registration"""
+    
+    form = RegisterUser()
 
-# **GET */register :*** Show a form that when submitted will register/create a user. This form should accept a username, password, email, first_name, and last_name. Make sure you are using WTForms and that your password input hides the characters that the user is typing!
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
 
-# **POST */register :*** Process the registration form by adding a new user. Then redirect to ***/secret***
+        new_user = User(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+        
+        db.session.add(new_user)
+        db.session.commit()
 
-# **GET */login :*** Show a form that when submitted will login a user. This form should accept a username and a password. Make sure you are using WTForms and that your password input hides the characters that the user is typing!
+        flash(f'Added {username}!', 'success')
+        return redirect('/')
+    else:
+        return render_template('register.html', form=form)
 
-# **POST */login :*** Process the login form, ensuring the user is authenticated and going to ***/secret*** if so.
-
-# **GET */secret :*** Return the text “You made it!” (don’t worry, we’ll get rid of this soon)
 
 
 
